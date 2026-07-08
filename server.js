5aad07e1f10926c7d8cc39e9503d46221dd14383
zaki <zakariaefaraji5@gmail.com>
2026-07-08 10:15:39 +0100

    serjer js

--- server.js
+++ server.js
@@ -16,7 +16,17 @@ app.use("/historique", require("./routes/historiqueRoutes"));
 app.use("/statistiques", require("./routes/statistiqueRoutes"));
 
 const PORT = process.env.PORT || 5000;
-
+app.get("/", (req, res) => {
+    res.json({
+        message: "Bon de Restauration API is running",
+        endpoints: [
+            "/personnes",
+            "/consommations",
+            "/historique",
+            "/statistiques"
+        ]
+    });
+});
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
